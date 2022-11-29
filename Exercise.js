492c5d98189037ff31c58321dc73e88008b54eef
ttornike1991 <100797221+ttornike1991@users.noreply.github.com>
2022-11-29 13:18:20 +0400

    Create Exercise.js

---
+++ Exercise.js
@@ -0,0 +1,17 @@
+
+let arr = [5, 15, 20, 60, 88, 6];
+
+let x = "gadamravlebuli ricvxvebi ";
+let y="gayofili ricxvebi ";
+
+for (let i = 0; i < arr.length; i++){
+    if (arr[i] > 10) {
+        x += arr[i] * 10+"//";
+    }
+
+    else {
+        y += arr[i] / 0.5+"//";
+    }
+}
+
+console.log( x,"\n",y)
